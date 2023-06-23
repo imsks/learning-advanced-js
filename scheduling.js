ec9360f3a9581f8db60147bad9610b3882540753
Sachin <sachinkshuklaoo7@gmail.com>
2023-06-23 11:07:13 +0530

    Scheduling

--- scheduling.js
+++ scheduling.js
@@ -1,36 +1,50 @@
-// // let timerId = setTimeout(function tick() {
-// //     console.log('Tick')
-// //     timerId = setTimeout(tick, 1000)
-// // }, 1000)
+// // // let timerId = setTimeout(function tick() {
+// // //     console.log('Tick')
+// // //     timerId = setTimeout(tick, 1000)
+// // // }, 1000)
 
-// // // With setTimeout
+// // // // With setTimeout
+// // // function printNumbers(from, to) {
+// // //     let curr = from
+
+// // //     let timeId = setTimeout(function counter() {
+// // //         console.log(curr)
+
+// // //         curr++
+
+// // //         if (curr > to) return
+
+// // //         timeId = setTimeout(counter, 1000)
+// // //     }, 1000)
+// // // }
+
+// // // printNumbers(2, 5)
+
+// // // With setInterval
 // // function printNumbers(from, to) {
 // //     let curr = from
 
-// //     let timeId = setTimeout(function counter() {
+// //     let timerId = setInterval(() => {
 // //         console.log(curr)
 
 // //         curr++
 
-// //         if (curr > to) return
-
-// //         timeId = setTimeout(counter, 1000)
+// //         if (curr > to) clearInterval(timerId)
 // //     }, 1000)
 // // }
 
 // // printNumbers(2, 5)
+// setTimeout(() => console.log("timeout"))
 
-// // With setInterval
-// function printNumbers(from, to) {
-//     let curr = from
+// Promise.resolve()
+//     .then(() => console.log("promise"))
 
-//     let timerId = setInterval(() => {
-//         console.log(curr)
+// console.log("code")
 
-//         curr++
-
-//         if (curr > to) clearInterval(timerId)
-//     }, 1000)
-// }
-
-// printNumbers(2, 5)
+1
+7
+3
+5
+4
+2
+6
