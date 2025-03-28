08b6354322b387bb281304f33f23b59a874f8e32
Sachin <sachinkshuklaoo7@gmail.com>
2025-03-28 10:13:57 +0530

    Refactor JavaScript examples and add explanations for closures, hoisting, and timer functions

--- front-end/index.js
+++ front-end/index.js
@@ -1,17 +1,117 @@
-// // 1. Difference between var, let, and const
-// function demo() {
-//     if (true) {
-//         var a = 10
-//         let b = 20
-//         const c = 30
-//     }
+// // // // 1. Difference between var, let, and const
+// // // function demo() {
+// // //     if (true) {
+// // //         var a = 10
+// // //         let b = 20
+// // //         const c = 30
+// // //     }
 
-//     console.log(a)
-//     console.log(b)
-//     console.log(c)
+// // //     console.log(a)
+// // //     console.log(b)
+// // //     console.log(c)
+// // // }
+
+// // // demo()
+
+// // // Hoisting - JavaScript “hoists” declarations to the top of their scope (but not initializations).
+// // // var is initialized as undefined but let is hoisted but not initialized.
+
+// // // Closure
+
+// // // function outer() {
+// // //     let count = 0
+
+// // //     return function inner() {
+// // //         count++
+// // //         console.log("Count is ", count)
+// // //     }
+// // // }
+
+// // // const counter = outer()
+
+// // // counter()
+// // // counter()
+
+// // // function createUser(name) {
+// // //     return {
+// // //         getName: () => name,
+// // //         changeName: (newName) => (name = newName)
+// // //     }
+// // // }
+
+// // // const user1 = createUser("Sachin")
+// // // console.log(user1.getName())
+
+// // // user1.changeName("Dhoni")
+// // // console.log(user1.getName())
+
+// // // Questions
+
+// // // 1.Classic Closure Trap
+
+// // // for (var i = 0; i < 3; i++) {
+// // //     setTimeout(() => console.log(i), 100)
+// // // }
+// // /**
+// //  * Output
+// //  *
+// //  */
+
+// // // // Q3: Timer Factory
+// // // function createTimer(name) {
+// // //     let seconds = 0
+
+// // //     return function tick() {
+// // //         seconds++
+// // //         console.log(`${name}: ${seconds}s`)
+// // //     }
+// // // }
+
+// // // const gameTimer = createTimer("Game")
+// // // gameTimer()
+// // // gameTimer()
+
+// // // function outer() {
+// // //     let a = 5
+
+// // //     return function inner() {
+// // //         console.log(a)
+// // //         a++
+// // //     }
+// // // }
+
+// // // const fn = outer()
+// // // fn() // 5
+// // // fn() // 5
+
+// // function createTimer(name) {
+// //     let seconds = 0
+
+// //     return function tick() {
+// //         seconds++
+// //         console.log("name ", name, " : ", "seconds ", seconds)
+// //     }
+// // }
+
+// // const quizTimer = createTimer("Quiz")
+// // setInterval(quizTimer, 1000)
+
+// // Implement once(fn)
+
+// function once(fn) {
+//     let called = false
+//     let result
+
+//     return function (...args) {
+//         if (!called) {
+//             result = fn.apply(this, args)
+//             called = true
+//         }
+
+//         return result
+//     }
 // }
 
-// demo()
-
-// Hoisting - JavaScript “hoists” declarations to the top of their scope (but not initializations).
-// var is initialized as undefined but let is hoisted but not initialized.
+// const init = once(() => console.log("Initialized"))
+// init()
+// init()
